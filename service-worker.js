568a195c9b3deaaf8be50b10d7b25057e17bfbb5
projetsjsl <projetsjsl@gmail.com>
2025-08-01 01:11:44 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,21 @@
 
-// service-worker.js
-
-const CACHE_NAME = 'reer-temi-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/simulateur.js',
-  '/graphique.js',
-  '/tableau.js',
-  '/tauxMarginauxCQFF.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png',
-  'https://cdn.jsdelivr.net/npm/chart.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('temi-cache').then(function(cache) {
+      return cache.addAll([
+        './simulateur_temi_ultra_plus_plus.html',
+        './manifest.json',
+        './icon-192x192.png',
+        './icon-512x512.png'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
